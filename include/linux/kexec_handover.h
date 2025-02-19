c2e3671d020cea96d2bc0cb9f92e33302e92b11d
Alexander Graf <graf@amazon.com>
2025-02-18 22:35:31 -0800

    kexec: add KHO parsing support
    When we have a KHO kexec, we get an FDT blob and scratch region to
    populate the state of the system. Provide helper functions that allow
    architecture code to easily handle memory reservations based on them and
    give device drivers visibility into the KHO FDT and memory reservations
    so they can recover their own state.
    
    Signed-off-by: Alexander Graf <graf@amazon.com>
    Co-developed-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Co-developed-by: Changyuan Lyu <changyuanl@google.com>
    Signed-off-by: Changyuan Lyu <changyuanl@google.com>

--- include/linux/kexec_handover.h
+++ include/linux/kexec_handover.h
@@ -47,6 +47,12 @@ int register_kho_notifier(struct notifier_block *nb);
 int unregister_kho_notifier(struct notifier_block *nb);
 
 void kho_memory_init(void);
+
+void kho_populate(phys_addr_t dt_phys, phys_addr_t scratch_phys,
+		  u64 scratch_len);
+const void *kho_get_fdt(void);
+void kho_return_mem(const struct kho_mem *mem);
+void *kho_claim_mem(const struct kho_mem *mem);
 #else
 static inline bool kho_is_enabled(void) { return false; }
 static inline void kho_init_node(struct kho_node *node) { }
@@ -65,6 +71,12 @@ static inline int register_kho_notifier(struct notifier_block *nb) { return 0; }
 static inline int unregister_kho_notifier(struct notifier_block *nb) { return 0; }
 
 static inline void kho_memory_init(void) {}
+
+static inline void kho_populate(phys_addr_t dt_phys, phys_addr_t scratch_phys,
+				u64 scratch_len) {}
+static inline void *kho_get_fdt(void) { return NULL; }
+static inline void kho_return_mem(const struct kho_mem *mem) { }
+static inline void *kho_claim_mem(const struct kho_mem *mem) { return NULL; }
 #endif /* CONFIG_KEXEC_HANDOVER */
 
 #endif /* LINUX_KEXEC_HANDOVER_H */
