1eea5a0ddb57dba802cce447558ba6aea98316c4
Mike Rapoport (Microsoft) <rppt@kernel.org>
2025-04-04 12:22:39 -0700

    kexec: add Kexec HandOver (KHO) memory preservation
    Introduce APIs allowing KHO users to preserve memory across kexec and
    get access to that memory after boot of the kexeced kernel
    
    kho_preserve_folio() - record a folio to be preserved over kexec
    kho_restore_folio() - recreates the folio from the preserved memory
    kho_preserve_phys() - record physically contiguous range to be
    preserved over kexec.
    
    The memory preservations are tracked by two levels of xarrays to manage
    chunks of per-order 512 byte bitmaps. For instance the entire 1G order
    of a 1TB x86 system would fit inside a single 512 byte bitmap. For
    order 0 allocations each bitmap will cover 16M of address space. Thus,
    for 16G of memory at most 512K of bitmap memory will be needed for order 0.
    
    At serialization time all bitmaps are recorded in a linked list of pages
    for the next kernel to process and the physical address of the list is
    recorded in KHO FDT.
    
    The next kernel then processes that list, reserves the memory ranges and
    later, when a user requests a folio or a physical range, KHO restores
    corresponding memory map entries.
    
    Suggested-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Co-developed-by: Changyuan Lyu <changyuanl@google.com>
    Signed-off-by: Changyuan Lyu <changyuanl@google.com>

---
+++ include/linux/kexec_handover.h
@@ -0,0 +1,55 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef LINUX_KEXEC_HANDOVER_H
+#define LINUX_KEXEC_HANDOVER_H
+
+#include <linux/types.h>
+
+struct folio;
+
+#define DECLARE_KHOSER_PTR(name, type) \
+	union {                        \
+		phys_addr_t phys;      \
+		type ptr;              \
+	} name
+#define KHOSER_STORE_PTR(dest, val)               \
+	({                                        \
+		typeof(val) v = val;              \
+		typecheck(typeof((dest).ptr), v); \
+		(dest).phys = virt_to_phys(v);    \
+	})
+#define KHOSER_LOAD_PTR(src)                                                 \
+	({                                                                   \
+		typeof(src) s = src;                                         \
+		(typeof((s).ptr))((s).phys ? phys_to_virt((s).phys) : NULL); \
+	})
+
+struct kho_serialization;
+
+#ifdef CONFIG_KEXEC_HANDOVER
+
+int kho_preserve_folio(struct kho_serialization *ser, struct folio *folio);
+int kho_preserve_phys(struct kho_serialization *ser, phys_addr_t phys,
+		      size_t size);
+struct folio *kho_restore_folio(phys_addr_t phys);
+#else
+
+static inline int kho_preserve_folio(struct kho_serialization *ser,
+				     struct folio *folio)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int kho_preserve_phys(struct kho_serialization *ser,
+				    phys_addr_t phys, size_t size)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline struct folio *kho_restore_folio(phys_addr_t phys)
+{
+	return NULL;
+}
+
+#endif /* CONFIG_KEXEC_HANDOVER */
+
+#endif /* LINUX_KEXEC_HANDOVER_H */
